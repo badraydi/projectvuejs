436ed286421bcda3daf263c653dd83d2fe667ce2
mahmoudammar9 <marwen.issaoui@sastec-group.com>
2022-10-26 16:08:21 +0100

    fix authentification

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,71 +1,104 @@
 <template>
-    <form @click.prevent>
-      <div class="g-3 align-item-center">
+  <form @click.prevent>
+    <div class="g-3 align-item-center">
       <h1>Login</h1>
       <div class="col-auto d-block mx-auto">
-          <input type="email" class="form-control" placeholder="Enter Your Email" v-model="state.email">
-          <span class="error-feedback" v-if="v$.email.$error">{{v$.email.$errors[0].$message}}</span>
+        <input
+          type="email"
+          class="form-control"
+          placeholder="Enter Your Email"
+          v-model="state.email"
+        />
+        <span class="error-feedback" v-if="v$.email.$error">{{
+          v$.email.$errors[0].$message
+        }}</span>
       </div>
-      <br>
+      <br />
       <div class="col-auto d-block mx-auto">
-          <input type="password" class="form-control" placeholder="Enter Your password" v-model="state.pass">
-          <span class="error-feedback" v-if="v$.pass.$error">{{v$.pass.$errors[0].$message}}</span>
+        <input
+          type="password"
+          class="form-control"
+          placeholder="Enter Your password"
+          v-model="state.pass"
+        />
+        <span class="error-feedback" v-if="v$.pass.$error">{{
+          v$.pass.$errors[0].$message
+        }}</span>
       </div>
-      <br>
+      <br />
       <div class="col-auto d-block mx-auto">
-          <button type="submit"  @click="userLogin()" class="btn btn-primary">Login</button>
+        <button type="submit" @click="userLogin()" class="btn btn-primary">
+          Login
+        </button>
       </div>
     </div>
-    </form>
-  </template>
+    <div class="g-3 align-item-center">
+      <div class="col-auto d-block mx-auto error-feedback">
+        {{ userNotFoundErr }}
+      </div>
+    </div>
+  </form>
+</template>
   
   <script>
-  import useValidate from '@vuelidate/core';
-  import { required, email } from "@vuelidate/validators";
-  import { reactive, computed } from 'vue';
-  export default {
-    name: "LogInPage",
-    //compostion API
-    setup(){
-      //data
-      const state = reactive({
-        pass:"",
-        email:"",
-      });
-      //validations
-      const rules = computed( () => {
-        return{
-          email: {required, email},
-          pass: {required},
-        }
-      });
-
-      const v$ = useValidate(rules, state);
+import axios from "axios";
+import useValidate from "@vuelidate/core";
+import { required, email } from "@vuelidate/validators";
+import { reactive, computed } from "vue";
+export default {
+  name: "LogInPage",
+  //compostion API
+  setup() {
+    //data
+    const state = reactive({
+      pass: "",
+      email: "",
+    });
+    //validations
+    const rules = computed(() => {
       return {
-        state, v$,
+        email: { required, email },
+        pass: { required },
       };
-    },
-    data(){
-      return{
+    });
 
+    const v$ = useValidate(rules, state);
+    return {
+      state,
+      v$,
     };
   },
-  methods:{
-    userLogin(){
+  data() {
+    return {
+      userNotFoundErr: "",
+    };
+  },
+  methods: {
+    async userLogin() {
       this.v$.$validate();
-      if(!this.v$.$error){
-          console.log("form Validated Successfully");
-      }else{
-          console.log("form Validation Failed");
+      if (!this.v$.$error) {
+        console.log("form Validated Successfully");
+        let result = await axios.get(
+          `http://localhost:3000/users?users?email=${this.state.email}&pass=${this.state.pass}`
+        );
+        if (result.status == 200 && result.data.length > 0) {
+          localStorage.setItem("users-info", JSON.stringify(result.data[0]));
+          this.$router.push({ name: 'home' });
+        } else {
+          this.userNotFoundErr = "User Not Found";
+        }
+        console.log(result);
+      } else {
+        console.log("form Validation Failed");
       }
     },
   },
-  };
-  </script>
+};
+</script>
   
 <style lang="scss" scoped>
-.error-feedback{
-  color:red;
+.error-feedback {
+  color: red;
   font-size: 0.85em;
 }
 </style>
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -9,7 +9,9 @@
           placeholder="Enter Your Name"
           v-model="name"
         />
-        <span class="error-feedback" v-if="v$.name.$error">{{v$.name.$errors[0].$message}}</span>
+        <span class="error-feedback" v-if="v$.name.$error">{{
+          v$.name.$errors[0].$message
+        }}</span>
       </div>
       <br />
       <div class="col-auto d-block mx-auto">
@@ -19,7 +21,9 @@
           placeholder="Enter Your Email"
           v-model="email"
         />
-        <span class="error-feedback" v-if="v$.email.$error">{{v$.email.$errors[0].$message}}</span>
+        <span class="error-feedback" v-if="v$.email.$error">{{
+          v$.email.$errors[0].$message
+        }}</span>
       </div>
       <br />
       <div class="col-auto d-block mx-auto">
@@ -29,11 +33,15 @@
           placeholder="Enter Your password"
           v-model="pass"
         />
-        <span class="error-feedback" v-if="v$.pass.$error">{{v$.pass.$errors[0].$message}}</span>
+        <span class="error-feedback" v-if="v$.pass.$error">{{
+          v$.pass.$errors[0].$message
+        }}</span>
       </div>
       <br />
       <div class="col-auto d-block mx-auto">
-        <button type="submit" @click="signUpNow()" class="btn btn-primary">Sign Up</button>
+        <button type="submit" @click="signUpNow()" class="btn btn-primary">
+          Sign Up
+        </button>
       </div>
     </div>
   </form>
@@ -56,27 +64,43 @@ export default {
   validations() {
     return {
       name: { required },
-      pass: { required, minLength:minLength(6)},
+      pass: { required, minLength: minLength(6) },
       email: { required, email },
     };
   },
-  methods:{
-  async signUpNow(){
-    this.v$.$validate();
-    if(!this.v$.$error){
-        console.log("form Validated Successfully");
-       let result = await axios.post('url','passParam');
-    }else{
+  methods: {
+    async signUpNow() {
+      this.v$.$validate();
+      if (!this.v$.$error) {
+        let result = await axios.post("http://localhost:3000/users", {
+          name: this.name,
+          email: this.email,
+          pass: this.pass,
+        });
+        if (result.status == 201) {
+          console.log("Added New User Successfully");
+          // save user data in local storage
+          localStorage.setItem("users-info", JSON.stringify(result.data));
+          console.log(result.data);
+          //redirect to home page
+          let user = localStorage.getItem("users-info");
+          if (user) {
+            this.$router.push({ name: 'Login' });
+          }
+        } else {
+          console.log("Error on Added New User");
+        }
+      } else {
         console.log("form Validation Failed");
-    }
+      }
+    },
   },
-},
 };
 </script>
 
 <style lang="scss" scoped>
-.error-feedback{
-  color:red;
+.error-feedback {
+  color: red;
   font-size: 0.85em;
 }
 </style>
